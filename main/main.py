bf323771ec0c11a4db00a638d452857ef5f82092
Lolfaceftw <ckcr20@gmail.com>
2023-06-26 19:26:29 +0800

    nick and port is now detected

--- main/main.py
+++ main/main.py
@@ -12,7 +12,11 @@ class ChatApp:
         self.is_connected = False
         self.receiver_ip = None
         self.receiver_port = None
-        self.nick = None
+        self.peer_ip = None
+        self.peer_port = None
+        self.peer_nick = None
+        self.nick = "Anon"
+        self.connected_peers = 0
     def send_message(self, event = None):
         message = message_entry.get()
         ### Message_entry.get() was used to retrieve what has been entered on the message box.
@@ -21,10 +25,10 @@ class ChatApp:
         if message.startswith("/connect"):
             connect_args = message.split()
             if len(connect_args) == 4:
-                self.receiver_ip = connect_args[1]
-                self.receiver_port = int(connect_args[2])
+                self.peer_ip = connect_args[1]
+                self.peer_port = int(connect_args[2])
                 self.nick = connect_args[3]
-                self.connect_to_peer(self.receiver_ip, self.receiver_port, self.nick)
+                self.connect_to_peer(self.peer_ip, int(self.peer_port))
 
         ### checking for the command. /connect. This is args /connect <ip> <port> <your_nickname>
         ### NOTICE: It would be better to connect first then set your IP to a certain nickname. Or easily,
@@ -34,7 +38,19 @@ class ChatApp:
                 message_box.insert(tk.END, "Invalid /connect command. Usage: /connect <ip> <port> <nickname>\n")
 
             ### We can change this else part.
-
+        elif message.startswith("/nick"):
+            if self.is_connected:
+                message_box.insert(tk.END, "You can't set a nickname while connected!")
+            else:
+                nick_args = message.split()
+                if len(nick_args) == 2:
+                    self.nick = nick_args[1]
+                    if self.is_connected:
+                        self.disconnect_from_peer(self.peer_ip, int(self.peer_port))
+                        self.connect_to_peer(self.peer_ip, int(self.peer_port))
+                    message_box.insert(tk.END, f"Nickname set to {self.nick}")
+                else:
+                    message_box.insert(tk.END, "Invalid /nick command. Usage: /nick <nickname>\n")
         elif message == "/peers":
             self.display_peers()
 
@@ -47,7 +63,7 @@ class ChatApp:
             # if len(disconnect_args) == 3:
             #     receiver_ip = disconnect_args[1]
             #     receiver_port = int(disconnect_args[2])
-            self.disconnect_from_peer(self.receiver_ip, self.receiver_port)
+            self.disconnect_from_peer(self.peer_ip, self.peer_port)
             # else:
             #     message_box.insert(tk.END, "Invalid /disconnect command. Usage: /disconnect <ip> <port>\n")
 
@@ -57,43 +73,52 @@ class ChatApp:
             if len(port_args) == 2:
                 self.receiver_port = int(port_args[1])
                 if self.is_connected:
-                    self.disconnect_from_peer(self.receiver_ip, self.receiver_port)
+                    self.disconnect_from_peer(self.peer_ip, self.peer_port)
                 message_box.insert(tk.END, f"Changed port to: {self.receiver_port}\n")
                 print(f"Changed port to: {self.receiver_port}\n")
             else:
                 message_box.insert(tk.END, "Invalid /port command. Usage: /port <port>\n")
         else:
+            if not message.startswith("\b/init"):
+                print(message)
             send_thread = threading.Thread(target=self.send_message_thread, args=(message,))
             send_thread.start()
 
         ### If there were no slash (/) commands, it will act as a message box nalang.
 
-    def connect_to_peer(self, receiver_ip, receiver_port, nickname):
+    def connect_to_peer(self, peer_ip, peer_port):
         try:
             conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             ### conn = to establish TCP connection (TCP socket)
-            conn.connect((receiver_ip, receiver_port))
+            conn.connect((peer_ip, peer_port))
             ### .connect() to establish a connection for the tuple IP and port.
-            connected_peers.append((receiver_ip, receiver_port, nickname))
+            conn.send(f"\b/init {self.receiver_ip} {self.receiver_port} {self.nick}".encode())
+            
             ### Simple appending of tuple. So IP-Port-Nickname
-            message_box.insert(tk.END, f"Connected to {nickname} ({receiver_ip}:{receiver_port})\n")
+            # message_box.insert(tk.END, f"Connected to {self.peer_nick} ({self.peer_ip}:{self.peer_port})\n")
             ### We can disregard the IP and Receiver port if nickname != ""
             self.update_peers()
-            receive_thread = threading.Thread(target=self.receive_messages, args=(conn, nickname))
+            receive_thread = threading.Thread(target=self.receive_messages, args=(conn, self.peer_nick))
             ### Multithreading, to receive messages.
             receive_thread.start()
             ### Initialization of multithread.
             self.is_connected = True
 
         except Exception as e:
-            error_message = f"Failed to connect to {nickname} ({receiver_ip}:{receiver_port}): {str(e)}"
+            error_message = f"Failed to connect to {self.peer_nick} ({self.peer_ip}:{self.peer_port}): {str(e)}"
             message_box.insert(tk.END, error_message + "\n")
 
             ### Error Handling
-
+    
+    def handle_init(self, init):
+        init = init.decode()
+        if init.startswith("\b/init"):
+            init = init[2:].split(" ")
+            self.peer_ip = init[1]
+            self.peer_port = init[2]
+            self.peer_nick = init[3]
     def disconnect_from_peer(self, receiver_ip, receiver_port):
         try:
-            connected_peer = None
             for peer in connected_peers:
                 ip, port, _ = peer
                 if ip == receiver_ip and port == receiver_port:
@@ -101,9 +126,9 @@ class ChatApp:
                     break
             ### FIFO pala siya. Naka FIFO to get the first person sa list Tapos yun ang ididisconnect.
 
-            if connected_peer:
-                nickname = connected_peer[2]
-                connected_peers.remove(connected_peer)
+            if connected_peers:
+                nickname = connected_peers[2]
+                connected_peers.pop(0)
                 message_box.insert(tk.END, f"Disconnected from {nickname} ({receiver_ip}:{receiver_port})\n")
                 self.is_connected = False
                 self.update_peers()
@@ -121,16 +146,23 @@ class ChatApp:
         ### This function updates the thread. Only if the sent message is for all.
         try:
             for peer in connected_peers:
-                receiver_ip, receiver_port, _ = peer
+                print(peer)
+                print(message)
+                peer_ip, peer_port, _ = peer
                 send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                send_socket.connect((receiver_ip, receiver_port))
+                send_socket.connect((peer_ip, int(peer_port)))
                 send_socket.sendall(message.encode())
                 send_socket.close()
 
-            sent_messages.append(f"Sent message to peers: {message}")
-            message_box.insert(tk.END, f"Sent message to peers: {message}\n")
+            if message.startswith("/"):
+                sent_messages.append(f"Command sent: {message}")
+                message_box.insert(tk.END, f"Command sent: {message}\n")
+            else:
+                sent_messages.append(f"Sent message to peers: {message}")
+                message_box.insert(tk.END, f"Sent message to peers: {message}\n")
         except Exception as e:
-            error_message = f"Failed to send message: {str(e)}"
+            print("error", message)
+            error_message = f"Failed to send message: {e}"
             message_box.insert(tk.END, error_message + "\n")
 
     def listen_thread(self):
@@ -138,24 +170,36 @@ class ChatApp:
         listen_socket.bind((self.receiver_ip, self.receiver_port))
         listen_socket.listen(3)
         
-        while True:
-            conn, address = listen_socket.accept()
-            receive_thread = threading.Thread(target=self.receive_messages, args=(conn, address))
-            receive_thread.start()
-    def receive_messages(self, conn, address, nickname = "Unknown"):
+        if not self.is_connected:
+            while True:
+                conn, address = listen_socket.accept()
+                if not self.is_connected:
+                    data = conn.recv(4096)
+                    self.handle_init(data)
+                    self.connect_to_peer(self.peer_ip, int(self.peer_port))
+                    self.is_connected = True
+                    message_box.insert(tk.END, f"{self.peer_ip}:{self.peer_port} ({self.peer_nick}) has joined the chat.\n")
+                    connected_peers.append((self.peer_ip, self.peer_port, self.peer_nick))
+                    if self.nick == "Anon":
+                        message_box.insert(tk.END, "We noticed you don't have a nickname. Set one now via /nick <nickname>.\n")
+                    
+                receive_thread = threading.Thread(target=self.receive_messages, args=(conn, address))
+                receive_thread.start()
+    def receive_messages(self, conn, address):
         while True:
             try:
                 data = conn.recv(4096)
                 ### This is to convert the received things to bytes. 4096 is the size.
-                if data:
+                if data:    
                     received_message = data.decode() ### Decoding of bytes
                     received_messages.append(received_message) ### Received message is then used to be printed into the message box
-                    message_box.insert(tk.END, f"Message received from {address[0]}:{address[1]} ({nickname}): {received_message}\n")
+
+                    if not received_message.startswith("\b/init"): message_box.insert(tk.END, f"Message received from {self.peer_ip}:{self.peer_port} ({self.peer_nick}): {received_message}\n")
                 else:
                     break
 
             except Exception as e:
-                error_message = f"Failed to receive message from {address[0]}:{address[1]} ({nickname}): {str(e)}"
+                error_message = f"Failed to receive message from {self.peer_ip}:{self.peer_port} ({self.peer_nick}): {str(e)}"
                 message_box.insert(tk.END, error_message + "\n")
                 break
 
@@ -207,6 +251,7 @@ message_box = tk.Text(window)
 message_box.pack()
 message_box.insert(tk.END, f"Your IP Address: {chat.receiver_ip}\n")
 message_box.insert(tk.END, f"Listening at Port {chat.receiver_port}\n")
+message_box.insert(tk.END, f"You don't have a nick! Set one now via /nick <nickname> or you will be called Anon.\n")
 peers_box = tk.Text(window, height=6, width=30)
 peers_box.configure(state="disabled")
 
